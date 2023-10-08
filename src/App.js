eb61a8343f438ef8837b073df2e69a6783587c53
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-08 11:35:51 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
-
+import { BrowserRouter as Router, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import SignupPage from './Pages/Signup';
 /**
  * ?  =====Import Components=====
  */
@@ -9,7 +10,14 @@ import Home from './Pages/Home';
 function App() {
   return (
     <div>
+      <Router>
+      <Route exact path="/" >
       <Home />
+      </Route>
+      <Route path="/signup">
+      <SignupPage />
+      </Route>
+      </Router>
     </div>
   );
 }
