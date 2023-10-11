36447479d4c657053e2865becb2ce2fd6e2018c2
Sreerag Mohan <sreeraagmohancv@gmail.com>
2023-10-11 17:17:23 +0530

    View Post updated

--- src/Components/View/View.js
+++ src/Components/View/View.js
@@ -1,33 +1,45 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
 import './View.css';
-import { AuthContext, FireBaseContext } from '../../store/CreateContext';
-import { postContext } from '../../store/DetailsContext'
+import { postContext } from '../../store/DetailsContext';
+import { FireBaseContext } from '../../store/CreateContext';
 
 function View() {
 
+const [userDetails,setUserDetails] = useState()
+const {postDetails} = useContext(postContext)
+const {Firebase} = useContext(FireBaseContext)
 
-  const { postDetails } = useContext(postContext);
+useEffect(()=>{
+  const {userId} = postDetails
+  Firebase.firestore().collection('users').where('id','==',userId).get().then((res)=>{
+    res.forEach(doc => {
+    setUserDetails(doc.data())
+    console.log(postDetails);
+    });
+  })
+},[])
 
   return (
     <div className="viewParentDiv">
       <div className="imageShowDiv">
         <img
-          src="../../../Images/R15V3.jpg"
+          src={postDetails ? postDetails.url : null}
           alt=""
         />
       </div>
       <div className="rightSection">
         <div className="productDetails">
-          <p>&#x20B9; 250000 </p>
-          <span>YAMAHA R15V3</span>
-          <p>Two Wheeler</p>
-          <span>Tue May 04 2021</span>
+          <p>&#x20B9; {postDetails ? postDetails.price : null} </p>
+          <span>{postDetails ? postDetails.name : null}</span>
+          <p>{postDetails ? postDetails.category : null}</p>
+          <span>{postDetails ? postDetails.createdAt : null}</span>
         </div>
-        <div className="contactDetails">
+       {userDetails && <div className="contactDetails">
           <p>Seller details</p>
-          <p>No name</p>
-          <p>1234567890</p>
-        </div>
+          <p>{userDetails.username}</p>
+          <p>{userDetails.phone}</p>
+        </div> }
       </div>
     </div>
   );
